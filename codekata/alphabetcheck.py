71918b16bf15ee70e51e49852bb0b547e2a5f6bc
krishsharan <krishsharan1998@gmail.com>
2019-06-12 12:09:08 +0530

    Create alphabetcheck.py

---
+++ codekata/alphabetcheck.py
@@ -0,0 +1,7 @@
+word=input()
+vowel=['a','b', 'c', 'd','e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't','u', 'v', 'w','x','y','z']
+if(word in vowel):
+ print('Alphabet')
+else:
+ print('No')
+ 
