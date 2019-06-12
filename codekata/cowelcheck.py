397101254c31b6523c18d64a64cde7e3b522f50e
krishsharan <krishsharan1998@gmail.com>
2019-06-12 11:58:53 +0530

    Create cowelcheck.py

---
+++ codekata/cowelcheck.py
@@ -0,0 +1,10 @@
+word=input()
+vowel=['a','e','i','o','u']
+consonant=  ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z']
+if(word in vowel):
+ print('Vowel')
+elif(word in consonant):
+ print('consonant')
+else:
+ print('Invalid')
+ 
