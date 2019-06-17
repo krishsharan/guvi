245655a6786c69a11602473c1e053f57b865fc3f
krishsharan <krishsharan1998@gmail.com>
2019-06-17 22:22:56 +0530

    Update num_word.py

--- codekata/num_word.py
+++ codekata/num_word.py
@@ -1,4 +1,4 @@
 number=int(input())
-word=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten]
+word=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
 if (1 <= number <= 10):
  print(word[number])
