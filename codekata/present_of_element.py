4931bc3d876728f4b0636369dfe9e77728912046
krishsharan <krishsharan1998@gmail.com>
2019-06-19 00:03:09 +0530

    Create present_of_element.py

---
+++ codekata/present_of_element.py
@@ -0,0 +1,11 @@
+x1,x2=map(int,input().split())
+yy=list(map(int,input().split()))
+count=0
+if(len(yy)==x1):
+ for i in yy:
+  if(i==x2):
+   count=1
+   if(count==1):
+    print('yes')
+   else: 
+    print('no') 
