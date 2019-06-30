9fb4a1f18139fbd80934d87d6b8e57f5e661a925
krishsharan <krishsharan1998@gmail.com>
2019-06-30 22:19:40 +0530

    Create str_vowel.py

---
+++ codekata/str_vowel.py
@@ -0,0 +1,7 @@
+xx=str(input())
+for x in xx:
+ if( x=='a' or x=='A' or x=='e' or x=='E' or x=='i' or x=='I' or x=='o' or x=='O' or x=='u' or x=='U'):
+  print('yes')
+  break
+ else:
+  print('no')
